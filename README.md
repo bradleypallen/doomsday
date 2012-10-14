195fecd6ea1570da5bffc9b918fa4ec5b450e240
Bradley P. Allen <bradley.p.allen@gmail.com>
2012-10-14 15:07:53 -0700

    Initial commit.
    Added working version of doomsday.py, README and LICENSE.

---
+++ README.md
@@ -0,0 +1,59 @@
+# doomsday.py: a Doomsday rule trainer
+
+## Overview
+A simple command line utility implemented in Python for practicing mental calculation of the day of the week for dates in the Gregorian calendar, using the Doomsday rule algorithm originally described by John Horton Conway [1]. This implementation relies on recent improvements to the algorithm by Chamberlain Fong and Michael K. Walters [[2]] that reduce the complexity of the mental calculation as well as the formula for calculating the anchor day for a century described in the Wikipedia entry for the Doomsday rule [[3]].
+
+## Requirements
+
+Python 2.6 or later.
+
+## Installation
+
+    $ git clone https://github.com/bradleypallen/doomsday.git
+    $ cd doomsday
+    $ chmod +x *.py
+    
+## Usage
+
+    $ ./doomsday.py
+    Type 'q' to quit, 0 - 6 for day of week guess.
+	December 9th, 1917? 3
+	Wrong!
+	1917 is in the 20 century.
+	doomcentury = ((5*20 + floor(19/4)) mod 7 + Thursday) mod 7
+	            = ((100 + 4) mod 7 + Thursday) mod 7
+	            = (104 mod 7 + 4) mod 7
+	            = (6 + 4) mod 7
+	            = 10 mod 7
+	            = 3
+	1917 is the 17th year in the 20th century.
+	Let x = 17.
+	17 is odd, so set x = x + 11 = 28
+	28 / 2 = 14.
+	doomsyear = (7 - (14 mod 7)) mod 7 = (7 - 0) mod 7 = 7 mod 7 = 0
+	doomsmonth = (9 - 12) = -3
+	day of week = (doomscentury + doomsyear + doomsmonth) mod 7 = (3 + 0 + -3) mod 7 = 0
+	April 9th, 2000? 0
+	Correct!
+	May 26th, 1679? q
+	Accuracy = 50% over 2 trials
+	$
+
+
+# License
+
+This code is provided under the terms of an MIT License [[4]]. See the LICENSE file for the copyright notice.
+    
+## Bibliography
+
+[1] Berlekamp, E.R., Conway, J.H. and Guy, R. K. Winning Ways for your Mathematical Plays. Volume 2: Games In Particular. Academic Press, NY (1982).
+
+[[2]] Fong, C. and Walters, M.K. Methods for Accelerating Conway's Doomsday Algorithm (part 2). ICIAM (2011).
+
+[[3]] Wikipedia. Doomsday rule. Downloaded from http://en.wikipedia.org/wiki/Doomsday_rule (2012).
+
+[[4]] Open Source Initiative (OSI). The MIT License. <i>Downloaded from http://www.opensource.org/licenses/mit-license.php</i> (2012).
+
+[2]: http://arxiv.org/pdf/1010.0765v4.pdf
+[3]: http://en.wikipedia.org/wiki/Doomsday_rule
+[4]: http://www.opensource.org/licenses/mit-license.php
---
+++ doomsday.py
@@ -0,0 +1,183 @@
+#!/usr/bin/env python
+
+# doomsday.py: a command line trainer for the mental calculation
+# of the day of the week for any date in the Gregorian calendar,
+# using the Doomsday algorithm original developed by John Horton Conway.
+
+import datetime, calendar, math, random
+
+# Given that the Doomsday algorithm works for the Gregorian calendar,
+# we refrain from training on dates earlier than the date of its earliest
+# adoption, October 15th, 1582.
+OCTOBER_15TH_1582 = datetime.date(1582, 10, 15).toordinal()
+
+# Additionally, we arbitrary limit training to dates no later than 
+# the last day of the year 2600.
+DECEMBER_31ST_2600 = datetime.date(2600, 12, 31).toordinal()
+
+# From http://stackoverflow.com/questions/739241/python-date-ordinal-output,
+# a routine for providing an ordinal description of the day of the date.
+def ordinal(n):
+    if 10 <= n % 100 < 20:
+        return str(n) + 'th'
+    else:
+        return  str(n) + {1 : 'st', 2 : 'nd', 3 : 'rd'}.get(n % 10, "th")
+
+# Since datetime.strftime() doesn't handle years earlier than 1900, we provide own date string function.
+def date_str(date):
+    return "%s %s, %d" % (calendar.month_name[date.month], ordinal(date.day), date.year)
+
+# Returns True if year is a leap year, used in the calculation of doomsmonth().
+def leapyear(year):
+    if year % 400 == 0:
+        return True
+    elif year % 100 == 0:
+        return False
+    elif year % 4 == 0:
+        return True
+    else:
+        return False
+
+# The first term in the doomsday.day_of_week() calculation, using
+# the formula given in http://en.wikipedia.org/wiki/Doomsday_rule#Finding_a_century.27s_anchor_day.
+def doomscentury(date):
+    thursday = 4
+    c = int(str(date.year)[0:2]) + 1
+    return ((((5 * c) + ((c - 1) / 4)) % 7) + thursday) % 7
+
+# The second term in the doomsday.day_of_week() calculation, using
+# Fong and Walters' Odds+11 method from http://arxiv.org/abs/1010.0765.
+def doomsyear(date):
+    x = int(str(date.year)[2:])
+    if x % 2 == 1:
+        x = x + 11
+    x = x / 2
+    if x % 2 == 1:
+        x = x + 11
+    x = x % 7
+    return (7 - x) % 7
+
+# The third term in the doomsday.day_of_week() calculation, calculating
+# the difference in days between the day of the month in question and
+# a Doomsday during the given month.
+def doomsmonth(date):
+    # In January and February, the doomsday used depends
+    # on whether or not the year of the date is a leap year.
+    if date.month == 1:
+        if leapyear(date.year):
+            return date.day - 11
+        else:
+            return date.day - 10
+    elif date.month == 2:
+        if leapyear(date.year):
+            return date.day - 22
+        else:
+            return date.day - 21
+    # In March we use the 7th as our reference Doomsday.
+    elif date.month == 3:
+        return date.day - 7
+    # Even months after March use the day of the month
+    # equal to the month of the year
+    elif date.month % 2 == 0:
+        return date.day - date.month
+    # For the remaining months, we use the "9 to 5 at the 7-11"
+    # mnemonic due to Conway
+    elif date.month == 5:
+        return date.day - 9
+    elif date.month == 9:
+        return date.day - 5
+    elif date.month == 7:
+        return date.day - 11
+    else: # date.month == 11
+        return date.day - 7
+
+# The key equation for the Doomsday algorithm per Fong and Walters in
+# http://arxiv.org/abs/1010.0765.
+def day_of_week(date):
+    return (doomscentury(date) + doomsyear(date) + doomsmonth(date)) % 7
+
+# Verbosely run through the details of the calculation implemented
+# by doomsday.day_of_the_week(), for use in training.
+def explain_day_of_week(date):
+    dc = doomscentury(date)
+    dy = doomsyear(date)
+    dm = doomsmonth(date)
+    dow = day_of_week(date)
+    c = int(str(date.year)[0:2]) + 1
+    print "%d is in the %d century." % (date.year, c)
+    print "doomcentury = ((5*%d + floor(%d/4)) mod 7 + Thursday) mod 7" % (c, c-1)
+    print "            = ((%d + %d) mod 7 + Thursday) mod 7" % (5*c, (c-1)/4)
+    print "            = (%d mod 7 + 4) mod 7" % (5*c+(c-1)/4)
+    print "            = (%d + 4) mod 7" % ((5*c+(c-1)/4)%7)
+    print "            = %d mod 7" % (((5*c+(c-1)/4) % 7)+4)
+    print "            = %d" % dc
+    x = int(str(date.year)[2:])
+    print "%d is the %s year in the %s century." % (date.year, ordinal(x), ordinal(c))
+    print "Let x = %d." % x
+    if x % 2 == 1:
+        print "%d is odd, so set x = x + 11 = %d" % (x, x + 11)
+        x = x + 11
+    print "%d / 2 = %d." % (x, x/2)
+    x = x / 2
+    if x % 2 == 1:
+        print "%d is odd, so set x = x + 11 = %d" % (x, x + 11)
+        x = x + 11
+    print "doomsyear = (7 - (%d mod 7)) mod 7 = (7 - %d) mod 7 = %d mod 7 = %d" % (x, (x%7), (7 - (x%7)), dy)
+    if date.month == 1:
+        print "%d %s a leap year." % (date.year, "is" if leapyear(date.year) else "is not")
+        if leapyear(date.year):
+            print "doomsmonth = (%d - 11) = %d" % (date.day, date.day - 11)
+        else:
+            print "doomsmonth = (%d - 10) = %d" % (date.day, date.day - 10)
+    elif date.month == 2:
+        print "%d %s a leap year." % (date.year, "is" if leapyear(date.year) else "is not")
+        if leapyear(date.year):
+            print "doomsmonth = (%d - 22) = %d" % (date.day, date.day - 22)
+        else:
+            print "doomsmonth = (%d - 21) = %d" % (date.day, date.day - 21)
+    elif date.month == 3:
+        print "doomsmonth = (%d - 7) = %d" % (date.day, date.day - 7)
+        return date.day - 7
+    elif date.month % 2 == 0:
+        print "doomsmonth = (%d - %d) = %d" % (date.day, date.month, date.day - date.month)
+    elif date.month == 5:
+        print "doomsmonth = (%d - 9) = %d" % (date.day, date.day - 9)
+    elif date.month == 9:
+        print "doomsmonth = (%d - 5) = %d" % (date.day, date.day - 5)
+    elif date.month == 7:
+        print "doomsmonth = (%d - 11) = %d" % (date.day, date.day - 11)
+    else: # date.month == 11
+        print "doomsmonth = (%d - 7) = %d" % (date.day, date.day - 7)
+    print "day of week = (doomscentury + doomsyear + doomsmonth) mod 7 = (%d + %d + %d) mod 7 = %d" % (dc, dy, dm, dow)
+
+# A command line loop for training a user in the mental calculation of
+# the day of the week using the Doomsday algorithm.
+# The loop prompts user for the day of the week for a random date;
+# if the user's answer is incorrect, print out an explanation of the correct calculation.
+# The user's accuracy during the session is printed out on exit.
+def main():
+    trials = 0
+    errors = 0
+    print "Type 'q' to quit, 0 - 6 for day of week guess."
+    while True:
+        date = datetime.date.fromordinal(random.randint(OCTOBER_15TH_1582, DECEMBER_31ST_2600))
+        input = raw_input("%s? " % date_str(date))
+        if input == 'q':
+            break
+        elif input.isdigit():
+            answer = int(input)
+            dow = day_of_week(date)
+            trials += 1
+            if answer == dow:
+                print "Correct!"
+            else:
+                errors += 1
+                print "Wrong!"
+                explain_day_of_week(date)
+        else:
+            print "Bad input: type 'q' to quit, 0 - 6 for day of week guess."
+    if trials > 0:
+        print "Accuracy = %s over %d trials" % ("{0:.0f}%".format((1. - ((float(errors) / float(trials)))) * 100), trials)
+
+if __name__ == '__main__':
+    main()
